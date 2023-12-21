7d745aae9bc0d212652ff65392240cd96ccd473c
Yosef Yan <145193433+yosefyan@users.noreply.github.com>
2023-12-21 10:19:57 +0200

    Add files via upload

--- js/loopCreator.js
+++ js/loopCreator.js
@@ -5,7 +5,7 @@ const imgsLoop = (container, array, ele, classes, shouldValue) => {
        <div class="flip-box bg-transparent w-full xl:w-[15vw]">
          <div class="flip-box-inner relative w-full h-[30vh] text-center ">
            <div class="flip-box-front bg-cyan-900/25 absolute w-full h-full">
-           <img class="h-full" src="../assets/questionMark.jpg" alt="question mark" />
+           <img class="h-full" src="./assets/questionMark.jpg" alt="question mark" />
            </div>
              <div class="flip-box-back absolute w-full h-full">
               <${ele} src="${arr}" alt="${arr}" class="${classes}">${
